f46a53fc2c8247e99f46f9c50dfb055b3c02300f
Tusk <Tuskman045@gmail.com>
2025-01-24 10:48:07 -0700

    Add project files.

---
+++ Simple_Calculator_MM/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Simple_Calculator_MM")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University")>
+<Assembly: AssemblyProduct("Simple_Calculator_MM")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ca300f99-2ad0-4839-8c0d-555f49e3464d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Simple_Calculator_MM/Simple_Calc.vb
@@ -0,0 +1,12 @@
+﻿'Malachi Marlow
+'Spring 2025
+'RCET2265
+'Simple Calculator
+'https://github.com/MalachiMarlow/SayMyName.git
+Module Simple_Calc
+
+    Sub Main()
+
+    End Sub
+
+End Module
